a8c6eb2652e7ad8ad09939706dd74dfe59599257
DMomplaisirASC <dmomplaisir@icloud.com>
2020-03-02 19:38:13 -0500

    Updated the default max size

--- wordpress/wp-content/themes/postlight-headless-wp/functions.php
+++ wordpress/wp-content/themes/postlight-headless-wp/functions.php
@@ -297,5 +297,7 @@ add_action( 'graphql_register_types', function() {
   }
 
   
-
+  @ini_set( 'upload_max_size' , '64M' );
+  @ini_set( 'post_max_size', '64M');
+  @ini_set( 'max_execution_time', '300' );
     
