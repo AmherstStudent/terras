f5ac6cf71ad7ffdab327eeba9bd7ee77d1bb2a0b
DMomplaisirASC <dmomplaisir@icloud.com>
2020-05-01 10:34:23 -0400

    format: embeds

---
+++ frontend/components/article/HTMLBlock.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from 'react'
+import styled from 'styled-components'
+
+interface HTMLProps {
+  html: string
+}
+const Wrapper = styled.div`
+  width: 100%;
+  max-height: 800px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+
+  overflow: overlay;
+`
+
+const HTMLBlock: FunctionComponent<HTMLProps> = (attributes: HTMLProps) => {
+  return <Wrapper dangerouslySetInnerHTML={{ __html: attributes.html }} />
+}
+
+export default HTMLBlock
