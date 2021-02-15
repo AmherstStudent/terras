325c9b0fdf62389e4010bdd0edf0af08639cdf35
DMomplaisirASC <dmomplaisir@icloud.com>
2021-02-15 11:17:22 -0500

    repapir

--- frontend/pages/sitemap.xml.tsx
+++ frontend/pages/sitemap.xml.tsx
@@ -1,4 +1,15 @@
 import gql from 'graphql-tag'
 import React from 'react'
+import { NextPage } from 'next'
 
 const REST_API = 'https://admin.amherststudent.com/posts'
+
+const Search: NextPage = () => {
+  return (
+    <>
+      <p>Hello</p>
+    </>
+  )
+}
+
+export default Search
