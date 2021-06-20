98ae3b9aa9160809e4007d26f6e608cd9e60d176
Dylan Momplaisir <dmomplaisir@icloud.com>
2021-06-20 01:08:17 -0400

    Added issue, and PR template

--- frontend/graphql/request.ts
+++ frontend/graphql/request.ts
@@ -1,3 +1,88 @@
+type Request = {query: string; variables: {[key: string]: unknown}}
+
+interface GQLError {
+  message: string;
+  locations?: { line: number; column: number }[];
+  path?: string;
+  extensions?: {
+    type?: string;
+  };
+}
+
+// This will be getting used at some point but the point is to clearly dictate timing, and jazz.
+interface GQLFetchDetails {
+  errors?: GQLError[];
+  request: Request;
+  duration: number;
+}
+
+interface GQLResponseBody {
+  data: unknown;
+  errors?: GQLError[];
+}
+
+const newFetchAPI = async (request: Request) => {
+  const headers = new Headers()
+
+  headers.append('Content-Type', 'application/json')
+
+
+  const res = await fetch(API_URL, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(request),
+  }).catch((e: Response) => {
+    // handle unexpected errors
+    report({ message: `${e.statusText || e.status}` }, { env: env(), element });
+    return Promise.reject(e);
+  });
+
+  const body = await res.json();
+
+  if (!body.data && !Array.isArray(body.errors)){
+    const errors = [{message: res.statusText, extensions: { type: res.status.toString() }}] as GQLError[];
+    
+    return {
+      data: null,
+      errors,
+    }
+  } 
+
+
+}
+
+/**
+ * Given a bit that's true if the response was a 200, and a parsed JSON
+ * representation of the response body, either returns the GraphQL
+ * response (from the "data" key) or throws an exception with the
+ * GraphQL errors attached.
+ */
+ function handleGraphqlResponse<T>(
+  ok: boolean,
+  json: GraphqlJsonResponse<T>
+): T {
+  if (ok && !json.errors && json.data) {
+    return json.data;
+  } else {
+    throw makeGraphQLError(json.message, json.errors);
+  }
+}
+function makeGraphQLError(
+  message: string | null | undefined,
+  errors: GraphqlErrorRecord[] = []
+): GraphqlError {
+  if (!message) {
+    message = `${errors.map(e => e.message).join(', ')}`;
+  }
+
+  const e = new Error(message) as GraphqlError;
+  e.errors = errors;
+  // We can use this as a signal to not send this error to an exception
+  // reporting service, since the server would have already sent it.
+  e.source = 'server';
+  return e;
+}
+
 export function gql(chunks: TemplateStringsArray, ...variables: any[]): string {
   return chunks.reduce((accumulator, chunk, index) => `${accumulator}${chunk}${index in variables ? variables[index] : ''}`, '')
 }
